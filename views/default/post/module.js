387958ad2ad17a3e01224a874aaafa7ec3569f22
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2018-03-26 15:54:19 +0200

    fix(modules): always prevent default click

--- views/default/post/module.js
+++ views/default/post/module.js
@@ -3,9 +3,7 @@ define(function(require) {
 	var $ = require('jquery');
 
 	$(document).on('click', '.elgg-module-collapse > .elgg-head', function(e) {
-		if ($(e.target).is('a')) {
-			e.preventDefault();
-		}
+		e.preventDefault();
 
 		$(this).closest('.elgg-module-collapse')
 			.toggleClass('elgg-state-collapsed elgg-state-expanded');
