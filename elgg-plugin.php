f583930ed51776edf2dbc8611cd3ff8607914a22
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2018-04-03 10:32:11 +0200

    fix(composer): add missing dependencies

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -1,6 +1,23 @@
 <?php
 
+$plugin_root = __DIR__;
+$root = dirname(dirname($plugin_root));
+$alt_root = dirname(dirname(dirname($root)));
+
+if (file_exists("$plugin_root/vendor/autoload.php")) {
+	$path = $plugin_root;
+} else if (file_exists("$root/vendor/autoload.php")) {
+	$path = $root;
+} else {
+	$path = $alt_root;
+}
+
 return [
+	'views' => [
+		'default' => [
+			'parsley.js' => $path . '/vendor/bower-asset/parsleyjs/dist/parsley.min.js',
+		]
+	],
 	'actions' => [
 		'cover/delete' => [
 			'controller' => \hypeJunction\Post\DeleteCoverAction::class,
