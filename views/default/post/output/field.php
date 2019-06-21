14df053ef54bfcdb34eda943b7cc037fed4e6859
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2019-06-21 11:19:34 +0200

    fix(output): normalize values before output

--- views/default/post/output/field.php
+++ views/default/post/output/field.php
@@ -10,10 +10,31 @@ if (!$value) {
 	return;
 }
 
+$prepare_value = function ($value, $params) {
+	switch ($params['#type']) {
+		case 'select' :
+			return $params['options_values'][$value];
+
+		case 'checkboxes' :
+		case 'radio' :
+			return array_search($value, $params['options']);
+
+		case 'checkbox' :
+			return (bool) $value ? elgg_echo('option:yes') : elgg_echo('option:no');
+	}
+
+	return $value;
+};
+
 if (!elgg_view_exists("output/$type")) {
 	if (is_array($vars['value'])) {
-		$vars['value'] = implode(', ', $vars['value']);
+		$vars['value'] = implode(', ', array_map(function ($value) use ($vars, $prepare_value) {
+			return $prepare_value($value, $vars);
+		}, $vars['value']));
+	} else {
+		$vars['value'] = $prepare_value($value, $vars);
 	}
+
 	$value = elgg_view('output/text', $vars);
 } else {
 	$value = elgg_view("output/$type", $vars);
