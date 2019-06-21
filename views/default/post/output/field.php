04f23d77fdf062df82faf79acf4d94a5a2785929
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2019-06-21 17:34:52 +0200

    fix(output): properly normalize radio values

--- views/default/post/output/field.php
+++ views/default/post/output/field.php
@@ -26,15 +26,16 @@ $prepare_value = function ($value, $params) {
 	return $value;
 };
 
-if (!elgg_view_exists("output/$type")) {
-	if (is_array($vars['value'])) {
-		$vars['value'] = implode(', ', array_map(function ($value) use ($vars, $prepare_value) {
-			return $prepare_value($value, $vars);
-		}, $vars['value']));
-	} else {
-		$vars['value'] = $prepare_value($value, $vars);
-	}
+if (is_array($vars['value'])) {
+	$vars['value'] = array_map(function ($value) use ($vars, $prepare_value) {
+		return $prepare_value($value, $vars);
+	}, $vars['value']);
+} else {
+	$vars['value'] = $prepare_value($value, $vars);
+}
 
+if (!elgg_view_exists("output/$type")) {
+	$vars['value'] = implode(', ', $vars['value']);
 	$value = elgg_view('output/text', $vars);
 } else {
 	$value = elgg_view("output/$type", $vars);
