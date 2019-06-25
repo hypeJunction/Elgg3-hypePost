2d102b0dadce7cd7154c572a474f077cba0f45d8
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2019-06-25 15:26:02 +0200

    fix(modules): use correct labels

--- views/default/input/profile_modules.php
+++ views/default/input/profile_modules.php
@@ -1,15 +1,23 @@
 <?php
 
 $value = elgg_extract('value', $vars);
+$entity = elgg_extract('entity', $vars);
+if (!$entity instanceof ElggEntity) {
+	return;
+}
+
+$modules = \hypeJunction\Post\Post::instance()->getModules($entity);
+
 $options = [];
 
-foreach ($value as $name => $module) {
-	$label = elgg_extract('label', $vars, elgg_echo("post:module:$name"));
+foreach ($modules as $name => $module) {
+	$label = elgg_extract('label', $module, elgg_echo("post:module:$name"));
 	$options[$label] = $name;
 }
 
 $vars['value'] = array_keys(array_filter($value));
 $vars['options'] = $options;
+$vars['switch'] = true;
 
 echo elgg_view('input/checkboxes', $vars);
 
